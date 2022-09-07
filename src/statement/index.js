a6548177c705eae32214de43a53073bfbfcff3be
Constantino <okonemmanuelmma@gmail.com>
2022-09-07 21:32:44 +0100

    deploying

--- src/statement/index.js
+++ src/statement/index.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./index.css";
 
 const Statement = () => {
-  return <div className="statement-root">hello world</div>;
+  return (
+    <div className="statement-root">
+      <div className="statement-main">
+        <div className="statement-left">
+          <div className="statement-fact">
+            <font>101 million children</font> in Africa are not in school
+          </div>
+          <div className="statement-note">
+            Of an estimated 101 million children not in school, more than half
+            are girls. However, this statistic increased when examining
+            secondary school education. In high-income countries, 95% as many
+            girls as boys attend primary and secondary schools. However, in
+            sub-Saharan Africa the figure is just 60% and 37 million children
+            receive extremely poor education.
+          </div>
+          <div className="statement-buttons">
+            <button>Learn more</button>
+            <button>Donate</button>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Statement;
